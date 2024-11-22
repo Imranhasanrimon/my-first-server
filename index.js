34fc0a2a8141de8fbe3acd983bbf50628d4e5969
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-22 22:41:05 +0600

    running my first code in expreess js

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const port = 5000
 app.get('/', (req, res) => {
     res.send('hello my first ever server')
 })
+
+app.listen(port, () => {
+    console.log(`my first server is runnit on port: ${port}`);
+})
