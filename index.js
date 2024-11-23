2bd1e5deb97a09f7d50d31690592973db701e799
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-23 08:07:40 +0600

    installing cors for non blocking data sharing from one port to another port

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const phones = require('./phones.json')
 const port = 5000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('hello my first ever server')
@@ -16,12 +19,12 @@ app.get('/phones', (req, res) => {
 })
 
 app.get('/phones/:id', (req, res) => {
-    const id = req.params.id;
-    const phone = phones.find(phone => phone.id == id);
+    const id = parseInt(req.params.id);
+    const phone = phones.find(phone => phone.id === id) || {};
     res.send(phone)
     console.log('i need data for id:', id);
 })
 
 app.listen(port, () => {
-    console.log(`my first server is runnit on port: ${port}`);
+    console.log(`my first server is running on port: ${port}`);
 })
